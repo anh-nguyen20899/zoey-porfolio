8b261ea1f0b716be3bf72437519c65052546957c
anhng <anananh1999@gmail.com>
2023-07-10 21:57:43 +1000

    organize sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
       <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
       <HeroSection />
-      <AboutSection />
       <ProjectsSection />
+      <AboutSection />
     </main>   
   )
 }
