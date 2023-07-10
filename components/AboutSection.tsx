efca2f63844e8b87e4ae4554ce6b52aaadd49319
anhng <anananh1999@gmail.com>
2023-07-10 20:45:32 +1000

    Config porfolio template

---
+++ components/AboutSection.tsx
@@ -0,0 +1,76 @@
+import React from "react"
+import Image from "next/image"
+
+const skills = [
+  { skill: "HTML CSS" },
+  { skill: "Tailwind CSS" },
+  { skill: "JavaScript" },
+  { skill: "TypeScript" },
+  { skill: "VueJs" },
+  { skill: "Angular 8+" },
+  { skill: "C# Dotnet" },
+  { skill: "Python" },
+  { skill: "Git" },
+  { skill: "GitHub" },
+]
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+              Get to know me!
+            </h1>
+            <p>
+              Hi, my name is Zoey and I am an{" "}
+              <span className="font-bold">{"ambitious"}</span>,
+              <span className="font-bold">{" self-motivated"}</span> software engineer
+              based in Sydney, NSW.
+            </p>
+            <br />
+            <p>
+            As a software engineer, I am familiar with browser testing, debugging, handling API requests and building beautiful UI layouts.
+            </p>
+            <br />
+            <p>
+            I am excited to see{" "}
+              <span className="font-bold text-teal-500"> new opportunities. 🙂
+              </span>{" "}
+            </p>
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                )
+              })}
+            </div>
+            <Image
+              src="/hero-image.png"
+              alt=""
+              width={325}
+              height={325}
+              className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
---
+++ components/ProjectsSection.tsx
@@ -0,0 +1,81 @@
+import React from "react"
+import Image from "next/image"
+import Link from "next/link"
+import SlideUp from "./SlideUp"
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
+
+const projects = [
+  {
+    name: "LearnIt Platform",
+    description:
+      "This platform is a web app that keeps track of lessons progress",
+    image: "/learnIt.png",
+    github: "https://github.com/anh-nguyen20899/learning-mern-project",
+    link: "https://sparkly-piroshki-ab82fb.netlify.app/login",
+  },
+  {
+    name: ".Net web api",
+    description: "I created a REST API project using the latest .NET 6 and MongoDb innovations.",
+    image: "/float-project-management.png",
+    github: "https://github.com/anh-nguyen20899/store_items",
+    link: "",
+  },
+]
+
+const ProjectsSection = () => {
+  return (
+    <section id="projects">
+      <h1 className="my-10 text-center font-bold text-4xl">
+        Projects
+        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+      </h1>
+
+      <div className="flex flex-col space-y-28">
+        {projects.map((project, idx) => {
+          return (
+            <div key={idx}>
+              <SlideUp offset="-300px 0px -300px 0px">
+                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                  <div className=" md:w-1/2">
+                    <Link href={project.link}>
+                      <Image
+                        src={project.image}
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        className="rounded-xl shadow-xl hover:opacity-70"
+                      />
+                    </Link>
+                  </div>
+                  <div className="mt-8 md:w-1/2">
+                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                      {project.description}
+                    </p>
+                    <div className="flex flex-row align-bottom space-x-4">
+                      <Link href={project.github} target="_blank">
+                        <BsGithub
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                      <Link href={project.link} target="_blank">
+                        <BsArrowUpRightSquare
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </SlideUp>
+            </div>
+          )
+        })}
+        
+      </div>
+    </section>
+  )
+}
+
+export default ProjectsSection
